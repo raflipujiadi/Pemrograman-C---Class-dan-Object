acbf62ee4c38eaa37493f97a3fc9ef0e11e1bfc4
raflipujiadi <pujiadirafli@gmail.com>
2020-03-21 22:41:09 +0700

    Pemrograman C# - Class dan Object

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TugasClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Mohammad Rafli Pujiadi";
+            taxi.OnDuty = true;
+            taxi.NumbOfPassenger = 2667;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOfPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
